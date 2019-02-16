ea93d5be163a477b169d8e6aa45e948bde4dea13
berialyue <berialyue@gmail.com>
2019-02-16 21:20:03 +0800

    add fastclick vue-lazyload axios

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
+import 'babel-polyfill'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VueLazyload from 'vue-lazyload'
+import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+
+fastClick.attach(document.body)
+Vue.use(VueAxios, axios)
+Vue.use(VueAwesomeSwiper)
+Vue.use(VueLazyload, {
+  loading: require('./common/images/default.png')
+})
 
 Vue.config.productionTip = false;
 
